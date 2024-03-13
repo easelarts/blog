e2257d50fb6494fa74c64756eed6de63551938bb
Jake Bailey <jake@jaketbailey.co.uk>
2024-03-13 12:45:49 +0000

    storyblok integration for blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,30 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import storyblok from '@storyblok/astro';
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 
 import tailwind from "@astrojs/tailwind";
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://easelartsbysarahelizabeth.co.uk',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: 'storyblok/BlogPost',
+        blogPostList: 'storyblok/BlogPostList',
+        page: 'storyblok/Page',
+      },
+      apiOptions: {
+        region: 'eu',
+      },
+    }),
+  ],
 });
