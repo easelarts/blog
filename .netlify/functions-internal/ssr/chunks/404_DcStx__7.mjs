13e9dca1347072a4ca567f243b9176b346029d5a
Jake Bailey <jake@jaketbailey.co.uk>
2024-03-14 18:28:51 +0000

    gitignore

--- .netlify/functions-internal/ssr/chunks/404_DcStx__7.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/404_Y-y4M5Iz.mjs').then(n => n._);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_DpIVGFly.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_BRegix9H.mjs').then(n => n.a);
-
-export { page };
